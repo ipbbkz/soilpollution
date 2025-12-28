25fc7cc43392f25eeb160823e2de718ca63d3653
Andrii Kurdiumov <kant2002@gmail.com>
2025-12-28 19:48:24 +0100

    Update dependencies

--- soilpollution/Program.cs
+++ soilpollution/Program.cs
@@ -77,30 +77,6 @@ app.MapBlazorHub();
 // Fallback to the server host page so Blazor routes work
 app.MapFallbackToPage("/_Host");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapAdditionalIdentityEndpoints();;
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
